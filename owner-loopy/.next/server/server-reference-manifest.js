a2545e2e13e14d8abd9f4a5fea32f48566e6739c
sunscarp <sanskarisamazing@gmail.com>
2026-06-08 23:23:50 +0530

    Remove node_modules

--- owner-loopy/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"gUHmQD7+iXTXYFPyl7leQc3llbodsV9IUEMgg0NmEPk=\"\n}"
--- owner-loopy/.next/static/chunks/src_app_sellers_page_tsx_785b328c._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_065b3f1b._.js",
-  "static/chunks/src_0b82e01a._.js"
-],
-    source: "dynamic"
-});
